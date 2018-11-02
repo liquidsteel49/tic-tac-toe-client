703d9e25025e9fbacdcb4fd3db5dda6b1f04a9b0
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-02 11:30:40 -0400

    gameboard spelling changed the spelling of gameBoard to gameboard in order to keep uniformity with HTML class of the same name

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,6 +1,6 @@
 // write function to pass submit form email as userName var (2.0)
 // build an array to store gameboard information
-const gameBoard = ['', '', '', '', '', '', '', '', '']
+const gameboard = ['', '', '', '', '', '', '', '', '']
 
 // for the future
 // let userName =  || 'guest' // username from submit box
