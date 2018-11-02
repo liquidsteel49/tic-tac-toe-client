c2d7924ba5c9cb94c14a00a586ee3689451efb79
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-02 11:24:48 -0400

    localhost changed changed local host to 7165

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,11 +1,13 @@
 'use strict'
 
+// api user to server comm
+
 let apiUrl
 const apiUrls = {
   production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  development: 'http://localhost:7165'
 }
-
+// do i need to chang below line because of localhost?
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
 } else {
