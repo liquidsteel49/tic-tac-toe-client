aebf80452b7213a6874ca6c5bdcdee140afe7a81
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-02 11:21:57 -0400

    gameboard and player added gameBoard var and currentPlayer var - need to set module exports

---
+++ assets/scripts/events.js
@@ -0,0 +1,12 @@
+// write function to pass submit form email as userName var (2.0)
+// build an array to store gameboard information
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+// for the future
+// let userName =  || 'guest' // username from submit box
+
+let currentPlayer = 'x'
+
+module.exports = {
+
+}
