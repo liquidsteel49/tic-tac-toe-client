136610225fc1e14c4b03b3294c89cae942517f33
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-02 14:38:06 -0400

    change 'x' to 'o' and back made changeToken function which will change the token used as the variable in the variable currentPlayer

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -7,6 +7,23 @@ const gameboard = ['', '', '', '', '', '', '', '', '']
 
 let currentPlayer = 'x'
 
-module.exports = {
+// put player token in box
 
+// document.findElementById().
+
+// change current player to x or o depending on clicking empty box
+const changeToken = function () {
+  if (currentPlayer === 'x') {
+    currentPlayer = 'o'
+  } else if (currentPlayer === 'o') {
+    currentPlayer = 'x'
+  }
+  return currentPlayer
+}
+
+/* check if valid click or turn off event
+listener once clicked */
+
+module.exports = {
+  changeToken
 }
